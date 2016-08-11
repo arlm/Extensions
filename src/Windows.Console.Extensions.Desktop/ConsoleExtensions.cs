5e68ca671cc67702cb2f53fe03af948b23716319
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-08-11 18:57:43 +0200

    Project updates

---
+++ src/Windows.Console.Extensions.Desktop/ConsoleExtensions.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) to owners found in https://github.com/arlm/Extensions/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace Windows.Console.Extensions
+{
+    public static class ConsoleExtensions
+    {
+    }
+}
