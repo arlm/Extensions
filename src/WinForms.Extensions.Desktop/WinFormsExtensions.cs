51faa8aad9d0b2cf6840c764b75c9ccf99a3967d
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-08-11 14:27:37 +0200

    Renamed WinForms.Extensions to WinForms.Extensions.Desktop
    Also added InvokeIfRequired extensions and some window/screen helpers
    like landscape detection and full screen window mode

---
+++ src/WinForms.Extensions.Desktop/WinFormsExtensions.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) to owners found in https://github.com/arlm/Extensions/blob/master/COPYRIGHT.md. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace WinForms.Extensions
+{
+    using System.ComponentModel;
+    using System.Windows.Forms;
+
+    public static class WinFormsExtensions
+    {
+        public static void EnterFullScreenMode(this Form form)
+        {
+            form.WindowState = FormWindowState.Normal;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.WindowState = FormWindowState.Maximized;
+            form.Bounds = Screen.PrimaryScreen.Bounds;
+        }
+
+        public static void InvokeIfRequired(this ISynchronizeInvoke obj, MethodInvoker action)
+        {
+            if (obj.InvokeRequired)
+            {
+                var args = new object[0];
+                obj.Invoke(action, args);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        public static bool IsScreenInLandscapeOrientation(this Form form)
+        {
+            int theScreenRectHeight = Screen.PrimaryScreen.Bounds.Height;
+            int theScreenRectWidth = Screen.PrimaryScreen.Bounds.Width;
+
+            // Compare height and width of screen and act accordingly.
+            return theScreenRectHeight < theScreenRectWidth;
+        }
+
+        public static bool IsScreenInPortraitOrientation(this Form form)
+        {
+            int theScreenRectHeight = Screen.PrimaryScreen.Bounds.Height;
+            int theScreenRectWidth = Screen.PrimaryScreen.Bounds.Width;
+
+            // Compare height and width of screen and act accordingly.
+            return theScreenRectHeight > theScreenRectWidth;
+        }
+
+        public static void LeaveFullScreenMode(this Form form)
+        {
+            form.FormBorderStyle = FormBorderStyle.Sizable;
+            form.WindowState = FormWindowState.Normal;
+        }
+    }
+}
