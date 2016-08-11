08ade60e8822398b416965da3cd9c437e078398a
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-08-11 19:01:16 +0200

    Renaming CLR.Extensions to CLR.Extensions.Portable

--- src/CLR.Extensions.Portable/Properties/AssemblyInfo.cs
+++ src/CLR.Extensions.Portable/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CLR.Extensions")]
+[assembly: AssemblyTitle("CLR.Extensions.Portable")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexandre Rocha Lima e Marcondes")]
