d60a1878d746f72207c417c54151aa5efbca5b34
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-08-11 14:28:19 +0200

    Renamed Windows.Services.Extensions to Windows.Services.Extensions.Desktop

--- src/Windows.Services.Extensions.Desktop/Properties/AssemblyInfo.cs
+++ src/Windows.Services.Extensions.Desktop/Properties/AssemblyInfo.cs
@@ -5,9 +5,7 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyTitle("Windows.Services.Extensions")]
+[assembly: AssemblyTitle("Windows.Services.Extensions.Desktop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexandre Rocha Lima e Marcondes")]
