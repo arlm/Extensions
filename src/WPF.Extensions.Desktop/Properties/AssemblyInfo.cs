96ddcdc10f220030f0ac7903aa310244febfc013
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2016-08-11 14:26:00 +0200

    Renaming WPF.Extensions to WPF.Extensions.Desktop
    Also added a DpiAwareWindow implementation

--- src/WPF.Extensions.Desktop/Properties/AssemblyInfo.cs
+++ src/WPF.Extensions.Desktop/Properties/AssemblyInfo.cs
@@ -5,9 +5,7 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyTitle("WPF.Extensions")]
+[assembly: AssemblyTitle("WPF.Extensions.Desktop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexandre Rocha Lima e Marcondes")]
